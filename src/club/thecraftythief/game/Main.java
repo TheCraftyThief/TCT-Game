7330fa81f7429581c6ac838a799a6d3fac15f615
DisabledMallis <mallisiscool@gmail.com>
2021-08-04 15:12:09 -0400

    Test ModelInteractEvent

--- src/club/thecraftythief/game/Main.java
+++ src/club/thecraftythief/game/Main.java
@@ -1,8 +1,13 @@
 package club.thecraftythief.game;
 
+import club.thecraftythief.engine.model.ModelData;
 import club.thecraftythief.engine.model.ModelMgr;
+import club.thecraftythief.engine.model.events.ModelInteractEvent;
 import club.thecraftythief.game.models.*;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -36,11 +41,22 @@ public class Main extends JavaPlugin implements Listener {
         ModelMgr.getInstance().registerModel(new TVModel());
         ModelMgr.getInstance().registerModel(new KeyModel());
 
+        Bukkit.getPluginManager().registerEvents(this, this);
+
         log.info("TCT-Game enabled");
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    @EventHandler
+    public void onModelInteract(ModelInteractEvent e) {
+        ModelData data = e.getModel();
+        ArmorStand stand = e.getEntity();
+        Player player = e.getPlayer();
+
+        player.sendMessage("Hi \""+player.getName()+"\", you clicked on a \""+data.getModelName()+"\" model! This model is held by the ArmorStand \""+stand.getUniqueId()+"\"!");
     }
 }
